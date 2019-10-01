b56ae08c6a56eb3cdeee3f552f813dbfc72dcb53
Shravan Meena <41790742+shravanmeena@users.noreply.github.com>
2019-10-01 18:26:53 +0530

    Add A Simple React App

---
+++ src/frameworks/complete-react-guide/src/Person/Hello.js
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const hello = () => {
+    return <p>I'm A Hello</p>
+};
+
+export default hello;
---
+++ src/frameworks/complete-react-guide/src/Person/Person.js
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const person = (props) => {
+     return <p>Hi I'm {props.name} and my Age is {props.age}</p>;
+};
+
+export default person;
