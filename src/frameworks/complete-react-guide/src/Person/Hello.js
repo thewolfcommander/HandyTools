ce3a10189baceb8c21c82c94b0cff13e64e8d53f
Manoj Tyagi <tyagimanojtyagi.22@gmail.com>
2019-10-13 23:32:53 +0530

    React app deleted

--- src/frameworks/complete-react-guide/src/Person/Hello.js
+++
@@ -1,7 +0,0 @@
-import React from 'react';
-
-const hello = () => {
-    return <p>I'm A Hello</p>
-};
-
-export default hello;
--- src/frameworks/complete-react-guide/src/Person/Person.js
+++
@@ -1,7 +0,0 @@
-import React from 'react';
-
-const person = (props) => {
-     return <p>Hi I'm {props.name} and my Age is {props.age}</p>;
-};
-
-export default person;
